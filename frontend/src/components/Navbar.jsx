2c0b28b57d1eae803bdf2b4793b8a88df9bac833
Azra <azracatovic13@gmail.com>
2026-06-17 22:06:05 +0200

    Add React Router and order model

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,33 @@
-function Navbar({cart}) {
+import { Link } from "react-router-dom";
+
+
+const token = localStorage.getItem("token");
+
+function Navbar({ cart }) {
   return (
     <nav className="navbar">
       <h2>PizzaHub 🍕</h2>
 
       <ul>
-        <li>Menu</li>
-        <li>Cart({cart.length})</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/">Menu</Link>
+        </li>
+
+        <li>
+          <Link to="/">Cart ({cart.length})</Link>
+        </li>
+
+        {token ? (
+  <li>Logout</li>
+) : (
+  <li>
+    <Link to="/login">Login</Link>
+  </li>
+)}
       </ul>
     </nav>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
